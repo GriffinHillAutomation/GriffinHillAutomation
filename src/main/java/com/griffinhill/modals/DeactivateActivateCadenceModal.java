5a16700ead68cf2bef3eef06d3d05175ffcad9f2
griffinhill_automation <josem@griffinhill.com>
2023-02-08 21:36:30 +0800

    added modal

---
+++ src/main/java/com/griffinhill/modals/DeactivateActivateCadenceModal.java
@@ -0,0 +1,30 @@
+package com.griffinhill.modals;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static com.griffinhill.site.pages.BasePageObject.*;
+import static com.griffinhill.utils.PageUtils.checkElementsEnabled;
+
+public class DeactivateActivateCadenceModal {
+
+    @FindBy(css = "div.ModalConfirmContent")
+    private static WebElement deactivateActivateCadence;
+    @FindBy(css = "div.ModalConfirmContent p")
+    private static WebElement deactivateActivateCadenceReminder;
+    @FindBy(xpath = "//button[text()='Cancel']")
+    private static WebElement cancel;
+    @FindBy(xpath = "//button[text()='Confirm']")
+    private static WebElement confirm;
+
+
+    public static void checkCadenceStatusModal(String message) {
+        deactivateActivateCadence.isDisplayed();
+        checkElementsEnabled(cancel, confirm);
+        verifyContent(deactivateActivateCadenceReminder, message);
+    }
+
+    public void confirm() { click(confirm); }
+
+    public void cancel() { click(cancel); }
+}
