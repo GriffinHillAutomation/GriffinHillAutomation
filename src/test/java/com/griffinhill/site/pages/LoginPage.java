c114ff3a2895320b724ff73cdedcdd04cd33cb6c
griffinhill_automation <josem@griffinhill.com>
2023-03-22 14:52:47 +0800

    optimize fw

--- src/test/java/com/griffinhill/site/pages/LoginPage.java
+++ src/test/java/com/griffinhill/site/pages/LoginPage.java
@@ -2,18 +2,21 @@ package com.griffinhill.site.pages;
 
 import com.griffinhill.entities.LoginInfo;
 import io.qameta.allure.Step;
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.testng.TestException;
 
 import static com.griffinhill.site.pages.BasePageObject.*;
 import static com.griffinhill.utils.PageUtils.checkElementsEnabled;
 
 public class LoginPage{
 
-    @FindBy(xpath = "//input[@name='username']")
+    @FindBy(xpath = "//input[@name='username']|//input[@label=\"Email address\"]")
     private static WebElement username;
-    @FindBy(xpath = "//input[@name='password']")
+    @FindBy(xpath = "//input[@name='password']|//input[@label='Password']")
     private static WebElement password;
     @FindBy(xpath = "//button[@type='submit']")
     private static WebElement login;
@@ -25,6 +28,7 @@ public class LoginPage{
     private static WebElement privacyPolicy;
     @FindBy(xpath = "//a[contains(text(), 'Integration Information')]")
     private static WebElement integrationInformation;
+
 
 
     public static void checkOpenedPage() {
@@ -39,9 +43,10 @@ public class LoginPage{
             sendKeys(password, loginInfo.getPassword());
             click(login);
         }
-        catch (ElementNotInteractableException e)
+        catch (Exception e)
         {
             waitForPageLoad();
+            throw new TestException("Log in Failed");
         }
 
         waitUntilPageIsLoaded();
--- src/test/java/com/griffinhill/tests/testcases/LoginTest.java
+++ src/test/java/com/griffinhill/tests/testcases/LoginTest.java
@@ -2,7 +2,11 @@ package com.griffinhill.tests.testcases;
 
 import com.griffinhill.entities.LoginInfo;
 import com.griffinhill.tests.TestsInit;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import reports.AllureListener;
+import io.qameta.allure.Description;
+
 
 import static com.griffinhill.site.pages.LoginPage.login;
 import static testdata.LoginDetails.loginCredentials;
