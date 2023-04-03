3a027124dc0a2e3a903e94c590884b7b7fcbf6c2
griffinhill_automation <josem@griffinhill.com>
2023-04-03 22:10:50 +0800

    added testcase to Scorecard

---
+++ src/test/java/com/griffinhill/tests/testcases/ScorecardBetaTest.java
@@ -0,0 +1,48 @@
+package com.griffinhill.tests.testcases;
+
+import com.griffinhill.entities.LoginInfo;
+import com.griffinhill.tests.TestsInit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.griffinhill.enums.PageNavigation.SCORE_CARD;
+import static com.griffinhill.site.pages.LoginPage.login;
+import static testdata.LoginDetails.loginCredentials;
+
+public class ScorecardBetaTest extends TestsInit{
+
+    LoginInfo user = loginCredentials();
+
+    @BeforeMethod
+    public void before() {
+        try {
+            login(user);
+            achievementHubPage.navigateTo(SCORE_CARD);
+            waitUntilPageIsLoaded();
+            scorecardPage.checkOpenedPage();
+        }
+        catch (Exception e)
+        {
+            //driver.close();
+            //driver.quit();
+            System.out.println("FAIL");
+        }
+    }
+
+    @Test
+    public void TCSC_001() {
+        scorecardBetaPage.validateScorecardPiplelineActive();
+    }
+
+    @Test
+    public void TCSC_002() {
+        scorecardBetaPage.clickContacts();
+        scorecardBetaPage.validateContactsAll();
+    }
+
+    @Test
+    public void TCSC_003() {
+        scorecardBetaPage.clickCalendar();
+        scorecardBetaPage.validateCalendar();
+    }
+}
